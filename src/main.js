b65b38f1b95e7c4c93d34e1ee0800de2e9cb57d7
lye <fklye0912@gmail.com>
2024-07-01 23:13:07 +0900

    vuetify 적용

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
+import vuetify from './plugins/vuetify'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(vuetify)
+
+app.mount('#app')

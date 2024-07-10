a3d6a07cca34c7b11e200b2bc594c32d3fc2fc07
lye <fklye0912@gmail.com>
2024-07-10 18:56:45 +0900

    tab 추가

--- src/view/mainView.vue
+++ src/view/mainView.vue
@@ -13,9 +13,29 @@
         <v-main class="d-flex flex-column">
             <v-container>
                 <!-- input -->
-                <v-card class="w-100 px-auto" height="90px">
-                    <v-card-text>
-                        <v-text-field v-model="input" class="mx-auto" width="50%" label="본캐 닉네임을 입력해주세요." append-inner-icon="mdi-magnify" @click:append-inner="onClickSearch"></v-text-field>
+                <v-card class="w-100 px-auto" height="200px">
+                    <v-tabs
+                        v-model="tab"
+                    >
+                        <v-tab value="auto">자동</v-tab>
+                        <v-tab value="manual">수동</v-tab>
+                    </v-tabs>
+                    <v-card-text class="mt-2">
+                        <v-tabs-window v-model="tab">
+                            <v-tabs-window-item value="auto">
+                            <v-text-field v-model="input" class="mx-auto" width="50%" label="본캐 닉네임을 입력해주세요." variant="solo" append-inner-icon="mdi-magnify" @click:append-inner="onClickSearch" @keyup.enter="onClickSearch"></v-text-field>
+                            <h3 class="font-weight-bold text-red-lighten-1">유니온 공격대에 추가되지 않은 캐릭터들은 조회되지 않습니다.</h3>
+                        </v-tabs-window-item>
+                        </v-tabs-window>
+                        <v-tabs-window v-model="tab">
+                            <v-tabs-window-item value="manual">
+                            <v-select width="25%" class="mx-auto" label="본캐 직업을 선택해주세요." variant="solo" density="compact"/>
+                            <v-btn class="mb-2" text="이미 키운 직업들을 선택" />
+                            <!-- <v-radio-group inline label="이미 키운 직업들을 선택해주세요." :density="compact" >
+                                <v-radio label="Radio One" value="one"></v-radio>
+                            </v-radio-group> -->
+                            </v-tabs-window-item>
+                        </v-tabs-window>
                     </v-card-text>
                 </v-card>
                 <!-- result -->
@@ -52,6 +72,7 @@
 <script setup>
 import { ref } from 'vue'
 
+const tab = ref(null)
 const input = ref('')
 
 const items = ref([
@@ -68,7 +89,7 @@ const items = ref([
 
 const resultCharactor = ref('직업이름')
 
-const onClickSearch = () => {
-    console.log('클릭')
+const onClickSearch = (e) => {
+    console.log(e)
 }
 </script>
